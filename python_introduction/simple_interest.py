1ec2a45e17fc8e45146e0e934f5a4b5f0c55c12c
NathyOne <nathanielabebe32@gmail.com>
2025-09-15 14:53:44 +0300

    naming correction

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,8 @@
 #calculating simple interest using python
 # I = P * R * T
 
-P = 1000
-R = 0.05
-T = 3
-I = P * R * T
+principal = 1000
+rate = 0.05
+time = 3
+I = principal * rate * time
 print("The simple interest is: ",I )
