ccf06a4f2efc25d0a0876ab856cd7d0d83a6d403
NathyOne <nathanielabebe32@gmail.com>
2025-09-15 15:12:34 +0300

    super minor change on naming

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,5 @@
 hours = 2
 seconds = 60
 minutes = 60
-hours_to_seconds = hours*(seconds*minutes)
-print("2 hour(s) is",hours_to_seconds,"seconds.")
+seconds = hours*3600
+print("2 hour(s) is",seconds,"seconds.")
