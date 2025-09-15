65a528652d351c4088a5354063b19314ff25610f
NathyOne <nathanielabebe32@gmail.com>
2025-09-15 14:33:32 +0300

    alx_basic_python_operation

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 =10
+number2 = 5
+sum = number1+number2
+diff = number1-number2
+product = number2*number1
+print("sum of 10 and 5 is " ,sum)
+print("difference of 10 and 5 is " ,diff)
+print("product of 10 and 5 is " ,product)
