e91217bec47a05226c97fa1a28d8d994a8b4abce
NathyOne <nathanielabebe32@gmail.com>
2025-09-22 11:43:58 +0300

    pattern drawing square

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,11 @@
-size = int(input("Enter the size of the pattern: "))
-max = size
-while size>0:
-    print("* "* max)
-    size = size-1
+# size = int(input("Enter the size of the pattern: "))
+# max = size
+# while size>0:
+#     print("* "* max)
+#     size = size-1
 
 
+while True:
+    size = int(input("Enter the size of the pattern: "))
+    for i in range(size):
+        print("* " * size)
