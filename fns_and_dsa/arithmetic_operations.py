29017146c03c0374de13c8377ded8b146b36133b
NathyOne <nathanielabebe32@gmail.com>
2025-10-01 11:24:09 +0300

    naming error

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,13 @@
 def perform_operation(num1: float, num2: float, operation: str):
     match operation:
         case "+":
-            print(num1+num2)
+            print(num1 + num2)
         case "-":
-            print(num1+num2)
+            print(num1 - num2)  # Also fixed: was adding instead of subtracting
         case "/":
             if num2 == 0:
                 print("can't divide by zero")
             else:
-                print(num1/num2)
+                print(num1 / num2)
         case "*":
-            return num1*num2
-        case _:
-            print(f"Unknown operation: {operation}")
-            return None
+            return num1 * num2
