f44c1a53bd6dfbac1f1533d51fd56341086d4261
NathyOne <nathanielabebe32@gmail.com>
2025-10-01 12:31:28 +0300

    from match-case to if/else

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            print(num1 + num2)
-        case "subtract":
-            print(num1 - num2)
-        case "divide":
-            if num2 == 0:
-                return
-            else:
-                print(num1 / num2)
-        case "multiply":
-            print(num1 * num2)
+
+    if operation == "add":
+        print(num1 + num2)
+    elif operation == "substract":
+        print(num1-num2)
+    elif operation == "multiply":
+        print(num1*num2)
+    elif operation == "divide":
+        if num2 == 0:
+            return print("can't divide by 0")
+    else:
+        return print("unknown operation")
