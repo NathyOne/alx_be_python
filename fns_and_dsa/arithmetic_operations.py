8e57e4e3c58f1a5110856eea73dabc6ecb9af5b6
NathyOne <nathanielabebe32@gmail.com>
2025-10-01 12:22:13 +0300

    small update

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
 def perform_operation(num1, num2, operation):
     match operation:
-        case "+":
+        case "add":
             print(num1 + num2)
-        case "-":
+        case "substract":
             print(num1 - num2)
-        case "/":
+        case "divide":
             if num2 == 0:
                 print("can't divide by zero")
             else:
                 print(num1 / num2)
-        case "*":
+        case "multiply":
             print(num1 * num2)
