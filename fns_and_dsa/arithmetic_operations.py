a6feff17fe459410f074d4e4c16fa10495c634a7
NathyOne <nathanielabebe32@gmail.com>
2025-10-01 12:23:07 +0300

    uuuf naming

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@ def perform_operation(num1, num2, operation):
     match operation:
         case "add":
             print(num1 + num2)
-        case "substract":
+        case "subtract":
             print(num1 - num2)
         case "divide":
             if num2 == 0:
