11711ece9b18fee8f21fb29119020f2099a3a533
NathyOne <nathanielabebe32@gmail.com>
2025-10-04 08:56:14 +0300

    some strict rule of alx evaluation method

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,9 +4,6 @@ import datetime
 from datetime import timedelta
 
 
-
-
-
 def display_current_datetime():
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {current_date}")
@@ -16,11 +13,11 @@ display_current_datetime()
 
 
 def calculate_future_date():
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.datetime.now()
     Added_time = int(
         input("Enter the number of days to add to the current date: "))
     future_date = current_time + timedelta(days=Added_time)
-    print(f"Future date: {future_date}")
+    print(f"Future date: {future_date.strftime("%Y-%m-%d %H:%M:%S")}")
 
 
 calculate_future_date()
