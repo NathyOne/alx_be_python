f824f598bcd487aed3d3e29b017eff5e87be9e9a
NathyOne <nathanielabebe32@gmail.com>
2025-10-04 09:40:51 +0300

    small fix

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,13 +2,22 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 
+# def convert_to_celsius(fahrenheit):
+#     result = (fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
+#     print(f"{fahrenheit}°F: {result}°C")
+
+
+# def convert_to_fahrenheit(celsius):
+#     result = (celsius+32)*CELSIUS_TO_FAHRENHEIT_FACTOR
+#     print(f"{celsius}°C: {result}°F")
+
 def convert_to_celsius(fahrenheit):
-    result = (fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
+    result = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     print(f"{fahrenheit}°F: {result}°C")
 
 
 def convert_to_fahrenheit(celsius):
-    result = (celsius+32)*CELSIUS_TO_FAHRENHEIT_FACTOR
+    result = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     print(f"{celsius}°C: {result}°F")
 
 
