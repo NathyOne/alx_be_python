0fbb1651f0a3b5adbad9b9ebcacfb3c4ae67c99d
NathyOne <nathanielabebe32@gmail.com>
2025-10-12 21:14:36 +0300

    naming in function definition for test oooh ALX!

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,26 +3,22 @@ from simple_calculator import SimpleCalculator
 
 
 class simple_calculator_test(unittest.TestCase):
-    def test_add(self):
+    def test_addition(self):
         calculator = SimpleCalculator()
         result = calculator.add(10, 5)
         self.assertEqual(result, 15)
 
-    def test_sub(self):
+    def test_subtraction(self):
         calculator = SimpleCalculator()
         result = calculator.subtract(10, 5)
         self.assertEqual(result, 5)
-    def test_divide(self):
+
+    def test_division(self):
         calculator = SimpleCalculator()
-        result = calculator.divide(10,5)
+        result = calculator.divide(10, 5)
         self.assertEqual(result, 2)
-    def test_mul(self):
+
+    def test_multiplication(self):
         calculator = SimpleCalculator()
         result = calculator.multiply(5, 10)
         self.assertEqual(result, 50)
-        
-
-
-
-
-
