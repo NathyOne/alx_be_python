73346aa130fd035d03a61cb087ede0c0ae918100
NathyOne <nathanielabebe32@gmail.com>
2025-10-12 21:47:59 +0300

    eliminating some lines

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,20 +5,16 @@ from simple_calculator import SimpleCalculator
 class simple_calculator_test(unittest.TestCase):
     def test_addition(self):
         self.calc = SimpleCalculator()
-        result = self.calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(self.calc.add(10, 5), 15)
 
     def test_subtraction(self):
         self.calc = SimpleCalculator()
-        result = self.calc.subtract(10, 5)
-        self.assertEqual(result, 5)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
 
     def test_division(self):
         self.calc = SimpleCalculator()
-        result = self.calc.divide(10, 5)
-        self.assertEqual(result, 2)
+        self.assertEqual(self.calc.divide(10, 5), 2)
 
     def test_multiplication(self):
         self.calc = SimpleCalculator()
-        result = self.calc.multiply(5, 10)
-        self.assertEqual(result, 50)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
