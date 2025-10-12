d8fa2d4242981d6fee07c4abb48e453445243ff1
NathyOne <nathanielabebe32@gmail.com>
2025-10-12 21:44:20 +0300

    naming error

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,21 +4,21 @@ from simple_calculator import SimpleCalculator
 
 class simple_calculator_test(unittest.TestCase):
     def test_addition(self):
-        calculator = SimpleCalculator()
-        result = calculator.add(10, 5)
+        self.calc = SimpleCalculator()
+        result = self.calc.add(10, 5)
         self.assertEqual(result, 15)
 
     def test_subtraction(self):
-        calculator = SimpleCalculator()
-        result = calculator.subtract(10, 5)
+        self.calc = SimpleCalculator()
+        result = self.calc.subtract(10, 5)
         self.assertEqual(result, 5)
 
     def test_division(self):
-        calculator = SimpleCalculator()
-        result = calculator.divide(10, 5)
+        self.calc = SimpleCalculator()
+        result = self.calc.divide(10, 5)
         self.assertEqual(result, 2)
 
     def test_multiplication(self):
-        calculator = SimpleCalculator()
-        result = calculator.multiply(5, 10)
+        self.calc = SimpleCalculator()
+        result = self.calc.multiply(5, 10)
         self.assertEqual(result, 50)
