9c0565c07fc25734e425459dd3e6257203e26dbf
NathyOne <nathanielabebe32@gmail.com>
2025-10-15 12:49:08 +0300

    polymorphism exercise

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Shape:
+    def area(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+    def area(self):
+        return f"The area of the Rectangle is {self.width*self.length}"
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return f"the area of the circle {math.pi*self.radius*self.radius}"
